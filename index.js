ff4e6a10f4edd636a3c09f93cd6103e1eded6136
Anthony Gawbill <gaw316@gmail.com>
2018-01-20 02:30:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,4 +22,9 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return ("I can't hear you!")
   }
+  
+  function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return ("I can't hear you!")
+  }
 }
