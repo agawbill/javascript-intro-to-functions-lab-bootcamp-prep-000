cec1d4af86037aa5929bd9ac05d90a664e92183f
Anthony Gawbill <gaw316@gmail.com>
2018-01-20 02:08:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+return string.toUpperCase()
+}
+
+function whisper(string) {
+return string.toLowerCase()
+}
+
+function whisper(string) {
+return string.toLowerCase()
+}
+
+function logShout(spy) {
+  console.log('HELLO')
+}
+
+function logWhisper(spy) {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+  whisper(string)
+}
